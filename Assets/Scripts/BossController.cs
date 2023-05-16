2828882c06e8a5fbbdb3974d4a92651d7651ca3b
H8872 <mm-laine@hotmail.fi>
2023-05-16 14:14:43 +0300

    add two different boss attack bases; closes #4, closes #7

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    [SerializeField] GameObject Slam;
+    [SerializeField] GameObject Slam, Projectile;
+    GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
-    void SlamGround()
+    void PlayerTargetedSlamGround(float delay)
     {
-        GameObject newSlam = Instantiate(Slam);
-        Destroy(newSlam, 1f);
+        SlamGround(player.transform.position, delay);
+    }
+
+    void SlamGround(Vector3 target, float delay = 0f)
+    {
+        if(target == Vector3.zero)
+            target = transform.position+Vector3.forward*2;
+        // make sure the gameobject has BossAttackBehaviour script
+        GameObject newSlam = Instantiate(Slam, target, transform.rotation);
+        BossAttackBehaviour behaviour = newSlam.GetComponent<BossAttackBehaviour>();
+        if(behaviour == null)
+        {
+            Debug.LogWarning("Add BossAttackBehaviour script, thanks");
+            Destroy(newSlam);
+        }
+        else
+        {
+            behaviour.hitDelay = delay;
+        }
+    }
+
+    void ShootProjectile(int type)
+    {
+        GameObject newProjectile = Instantiate(Projectile, transform.position + transform.forward * 2, transform.rotation);
+        if(newProjectile.GetComponent<BossAttackBehaviour>() == null)
+        {
+            Debug.LogWarning("Add BossAttackBehaviour script, thanks");
+            Destroy(newProjectile);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Time.time);
     }
 }
