50c5af62cd55228175522dfd932f8c4c5ce0396f
Jeesuz <juha.hietala53@gmail.com>
2023-05-16 11:08:12 +0300

    closes #1; added movement and simple melee

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerState
+    {
+        Idle,
+        Move,
+        Dodge,
+        Hurt
+    }
+
+    Vector3 displacement;
+    Rigidbody rb;
+    [SerializeField] float moveSpeed = 500f, meleeRange = 1f;
+    [SerializeField] GameObject melee;
+    bool playerMelee;
+    public PlayerState playerState = PlayerState.Idle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void MeleeAttack()
+    {
+        var newMelee = Instantiate(
+            melee, transform.position + transform.forward * meleeRange, transform.rotation
+            );
+        Destroy(newMelee, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMelee = Input.GetButtonDown("Melee");
+
+        if (playerMelee)
+        {
+            if (playerState == PlayerState.Idle || playerState == PlayerState.Move)
+            {
+                MeleeAttack();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (playerState == PlayerState.Idle || playerState == PlayerState.Move)
+        {
+            displacement = new Vector3(
+                Input.GetAxisRaw("Horizontal"), transform.position.y, Input.GetAxisRaw("Vertical")
+                );
+            displacement = Vector3.ClampMagnitude(displacement, 1);
+            rb.velocity = displacement * Time.deltaTime * moveSpeed;
+
+            if (rb.velocity.magnitude > 0)
+            {
+                playerState = PlayerState.Move;
+            } else {
+                playerState = PlayerState.Idle;
+            }
+
+
+
+        }
+    }
+}
