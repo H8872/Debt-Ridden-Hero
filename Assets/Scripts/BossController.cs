e8c3dbb97a64c39e433f0ece2a0e4bb81926c527
H8872 <mm-laine@hotmail.fi>
2023-05-16 11:24:59 +0300

    remove plane mesh collider and add rb on boss

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    [SerializeField] GameObject Slam;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void SlamGround()
+    {
+        GameObject newSlam = Instantiate(Slam);
+        Destroy(newSlam, 1f);
     }
 
     // Update is called once per frame
