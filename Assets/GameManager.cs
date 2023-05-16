373abb3b8f5089860b59dd2a2a777a75f092d461
H8872 <mm-laine@hotmail.fi>
2023-05-16 10:53:49 +0300

    made GameManager into a singleton

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(instance != null)
+        {
+            Debug.Log($"Only one {this} allowed. Deleting this {this}.");
+            Destroy(gameObject);
+        }
+        else
+            instance = this;
     }
 
     // Update is called once per frame
